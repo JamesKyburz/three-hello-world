e1d7323f0fb3d0010596bf8b2a2498c99f8217ab
James Kyburz <james.kyburz@gmail.com>
2016-06-28 20:05:45 +0200

    scale 100 for now

--- src/apply-model.js
+++ src/apply-model.js
@@ -1,6 +1,7 @@
 var THREE = require('three')
 
 module.exports = (container, app, model) => {
+  window.top.model = model
   container.add(model.scene)
   var view = calculateCamera(model.scene)
   app.camera.position.copy(view.position)
@@ -10,6 +11,7 @@ module.exports = (container, app, model) => {
 
 function calculateCamera (model, viewDirection) {
   viewDirection = viewDirection || new THREE.Vector3(-570.565, -2235.66, -961.08)
+  model.scale.copy(new THREE.Vector3(100, 100, 100))
   var box = getCompoundBoundingBox(model)
   var diagonal = box.size().length()
   var newDelta = viewDirection.clone()
